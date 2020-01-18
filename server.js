79a61f72bf29343505759a93f6cab240f9222a16
Sarah <sarkwon89@gmail.com>
2020-01-17 18:36:59 -0800

    redo hw

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var express = require("express");
 
-var PORT = process.env.PORT || 8081;
+var PORT = process.env.PORT || 8080;
 
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
-// Parse application body
+// Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -18,7 +18,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/burger_controller");
+var routes = require("./controllers/burgerController");
 
 app.use(routes);
 
